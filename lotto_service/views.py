2a80b3c8a8b5414dac9cf162ffccd55d8a92be79
Fellas <neuneu12@naver.com>
2025-04-17 00:49:21 +0900

    .

--- lotto_service/views.py
+++ lotto_service/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 import random
 
+# templates/lotto_service/index.html
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'lotto_service/index.html')
 
 def generate_numbers(request):
     numbers = sorted(random.sample(range(1, 46), 6))
-    bonus = random.randint(1, 45)
-    while bonus in numbers:
-        bonus = random.randint(1, 45)
     context = {
         'numbers': numbers,
-        'bonus': bonus
     }
-    return render(request, 'result.html', context)
+    return render(request, 'lotto_service/result.html', context)
